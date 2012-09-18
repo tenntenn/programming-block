550dd7709953f342853aba179d9e0a5e9e62a9c9
Takuya Ueda <uedatakuya@gmail.com>
2012-09-18 10:38:09 +0900

    テンプレートを追加した。

---
+++ template.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+    "fmt"
+    "text/template"
+    "flag"
+    "os"
+)
+
+func main() {
+    flag.Parse()
+
+    var pattern string
+    flag.StringVar(&pattern, "p", "templates/*.html", "template pattern like template/*.html")
+    template, err := template.ParseGlob(pattern)
+    if err != nil {
+        fmt.Fprintln(os.Stderr, err.Error())
+    }
+
+    var mainTemplate string
+    flag.StringVar(&mainTemplate, "t", "index", "main template like index")
+    template.ExecuteTemplate(os.Stdout, mainTemplate, nil)
+}
