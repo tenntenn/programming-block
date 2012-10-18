66ba7a77b0cf4ce6458bc476b40353594554b030
Takuya Ueda <uedatakuya@gmail.com>
2012-10-18 18:35:59 +0900

    ブロックのビューモデルを作った。 SVGのviewBoxを作成した。

--- js/view/block.js
+++ js/view/block.js
@@ -1,13 +1,14 @@
 (function() {
 
     /**
-     * Block viewmodel.
+     * ブロックのビューモデルです。
      * @constructor
      */
     sb.view.Block = function(name, model, x, y, col, row, panels) {
         var that = this;
         this.x = ko.observable(x);
         this.y = ko.observable(y);         
+        this.id = "block-"+name;
         this.rotate = ko.observable(0);
         this.width = function() {
             return col * sb.view.PANEL_SIZE;
@@ -20,19 +21,27 @@
             var rotate = "rotate("+this.rotate()+");";
             return translate + rotate;
         });
+        this.selected = ko.observable(false);
+        this.selectedFilter = ko.observable("filter-selected");
+        this.filter = ko.computed(function() {
+           if (that.selected()) {
+                return "url(#"+that.selectedFilter()+")";
+           }
+
+            return "";
+        });
     };
 
-
-    sb.view.renderBlocks(node, blocks) {
+    sb.view.renderBlocks(node, viewBox, blocks) {
         node.data(blocks)
             .enter()
             .append("svg:g")
             .classed("block", true)
             .attr("id", function(block) {
-                return "block-"+name;
+                return block.id;
             })
             .attr("data-bind", function(block) {
-                return "attr:{transform:transform}";
+                return "attr:{transform:transform, filter: filter}";
             })
             .attr("width", function(block) {
                 return block.width();
@@ -40,6 +49,38 @@
             .attr("height", function(block) {
                 return block.height();
             })
+            .on("click", function(block) {
+
+                var cx, cy;
+
+                // すでに選択されているものがあるか？
+                var isSelected = !(d3.selectAll(".selected").empty());
+                if (isSelected) {
+                    return;
+                }
+               
+                // 選択する 
+                d3.select(this)
+                  .classed("selected", true);
+                block.select(true); 
+
+
+                // 前面に持ってくる
+                d3.selectAll(".block")
+                  .sort(function(b1, b2) {
+                            if (block === b1) {
+                                return 1;
+                            }
+                            return 0;
+                        });
+                
+                // 中心に持ってくる
+                cx = block.x + block.width()  / 2;
+                cy = block.y + block.height() / 2;
+            })
+            .each(function(block) {
+                ko.applyBindings(block, d3.select(this)[0]);
+            });
     };
 
 })();
