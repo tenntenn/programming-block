905c43b76a7fa86a0a1fd34041cae16b904f3d79
Takuya Ueda <uedatakuya@gmail.com>
2013-01-08 13:59:37 +0900

    ディレクトリ構造を変更した。

--- js/model/connection.js
+++
@@ -1,9 +0,0 @@
-/**
- * 接続を表すクラス
- * @param {pb.model.Connector} pin
- * @param {pb.model.Connector} socket
- */
-pb.model.Connection = function(pin, socket) {
-    this.pin = pin;
-    this.socket = socket;
-};
